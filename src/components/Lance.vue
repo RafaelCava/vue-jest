e8de12108f52850445e8d961a3dba5e3d8918d54
Rafael Cavalcante <82680356+RafaelCava@users.noreply.github.com>
2022-05-29 15:30:50 -0300

    Develop (#1)
    * feat(lance): initial test unit
    
    * feat(coverage): insert script to create and update the coverage of tests in the code
    
    ]

--- src/components/Lance.vue
+++ src/components/Lance.vue
@@ -1,13 +1,13 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-  </div>
+  <h1>Hello</h1>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 
 export default defineComponent({
-  name: 'HomeView'
+  name: 'LanceComponent'
 })
 </script>
+
+<style></style>

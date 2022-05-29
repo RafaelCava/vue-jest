05d01733904fde5c6f3491f97a04c2982ff7b732
rafael cavalcante <ra.facavalcante@hotmail.com>
2022-05-29 15:27:17 -0300

    feat(coverage): insert script to create and update the coverage of tests in the code
    ]

--- tests/unit/Lance.spec.ts
+++ tests/unit/Lance.spec.ts
@@ -1,7 +1,7 @@
 import { mount } from '@vue/test-utils'
 import Lance from '@/components/Lance.vue'
 
-test('Não pode aceitar lance com valor menor que zero', () => {
+test('Não pode aceitar lances com valor menor que zero', () => {
   const wrapper = mount(Lance)
   expect(wrapper).toBeTruthy()
 })
